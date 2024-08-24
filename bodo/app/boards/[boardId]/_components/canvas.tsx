eb3b7188ec51e53569deff227997ac7c9a12ff78
Zakaria Chamakh <zakariachamakh32@gmail.com>
2024-08-24 17:41:25 +0100

    Added translate feature

--- bodo/app/boards/[boardId]/_components/canvas.tsx
+++ bodo/app/boards/[boardId]/_components/canvas.tsx
@@ -114,6 +114,36 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     );
   
 
+    const translateSelectedLayer = useMutation(
+      ({ storage, self }, point: Point) => {
+        if (canvasState.mode !== CanvasMode.Translating) return;
+  
+        const offset = {
+          x: point.x - canvasState.current.x,
+          y: point.y - canvasState.current.y,
+        };
+  
+        const liveLayers = storage.get("layers");
+  
+        for (const id of self.presence.selection) {
+          const layer = liveLayers.get(id);
+          if (layer) {
+            layer.update({
+              x: layer.get("x") + offset.x,
+              y: layer.get("y") + offset.y,
+            });
+          }
+        }
+  
+        setCanvasState({
+          mode: CanvasMode.Translating,
+          current: point,
+        });
+      },
+      [canvasState]
+    );
+  
+
     const resizeSelectedLayer = useMutation(
       ({ self, storage }, point: Point) => {
         if (canvasState.mode !== CanvasMode.Resizing) {
@@ -161,14 +191,16 @@ export const Canvas = ({ boardId }: CanvasProps) => {
     
           const current = pointerEventToCanvasPoint(e, camera);
 
-          if (canvasState.mode === CanvasMode.Resizing) {
+          if (canvasState.mode === CanvasMode.Translating) {
+            translateSelectedLayer(current);
+          } else if (canvasState.mode === CanvasMode.Resizing) {
             resizeSelectedLayer(current);
           }
     
     
           setMyPresence({ cursor: current });
         },
-            [canvasState, resizeSelectedLayer, camera]
+        [canvasState, resizeSelectedLayer, camera, translateSelectedLayer]
 
       );
       const handlePointerLeave = useMutation(({ setMyPresence }) => {
