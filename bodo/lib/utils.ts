17ccd742260a46cd5f9071808fd5bccaf46b5d9d
Zakaria Chamakh <zakariachamakh32@gmail.com>
2024-08-24 16:49:58 +0100

    Added cursors

--- bodo/lib/utils.ts
+++ bodo/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Camera } from "@/types/canvas";
+
 
 const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
@@ -8,4 +10,15 @@ export function cn(...inputs: ClassValue[]) {
 }
 export const connectionIdToColor = (connectionId: number) => {
   return COLORS[connectionId % COLORS.length];
+};
+
+
+export const pointerEventToCanvasPoint = (
+  e: React.PointerEvent,
+  camera: Camera
+) => {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
 };
